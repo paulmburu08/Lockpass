6a871d4a4bd0e0cd4130d847fb5265cf7cc8d215
paulmburu08 <paulmburu08@gmail.com>
2020-05-23 20:32:27 +0300

    add changes

---
+++ credentials.py
@@ -0,0 +1,13 @@
+class Lockpass:
+    '''
+    class that generate new instances of credentials
+    '''
+
+    credentials = []
+
+    def __init__(self,first_name,last_name,email,password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
