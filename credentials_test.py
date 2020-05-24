6abaf90b003e0f0d2ba2385062636337b257b35b
paulmburu08 <paulmburu08@gmail.com>
2020-05-25 00:05:42 +0300

    add changes

--- credentials_test.py
+++ credentials_test.py
@@ -10,7 +10,7 @@ class CredentialsTest(unittest.TestCase):
 
         self.new_credentials = Credentials('Twitter','paulmburu65','paulmburu08@gmail.com','python3465')
 
-     def tearDown(self):
+    def tearDown(self):
         '''
         tearDown method that does clean up after each test case has run.
         '''
@@ -29,7 +29,7 @@ class CredentialsTest(unittest.TestCase):
 
     def test_save_credentials(self):
         '''
-        test_save_credentials test case to test if the credentials object is saved into
+        test_save_credentials test case to test if the account credentials object is saved into
         the credentials list
         '''
 
@@ -38,11 +38,49 @@ class CredentialsTest(unittest.TestCase):
 
     def test_save_multiple_credentials(self):
         '''
-        test_save_multiple_credentials to check if we can save multiple credentials
+        test_save_multiple_credentials to check if we can save multiple account credentials
         objects to our credentials_list
         '''
 
         self.new_credentials.save_credentials()
         test_credentials = Credentials('Instagram','mburupavl','paulmburu08@gmail.com','think4562')
         test_credentials.save_credentials()
+
         self.assertEqual(len(Credentials.credentials_list),2)
+
+    def test_delete_credentials(self):
+        '''
+        test_delete_credentials to test if we can remove and account credentials from our credentials list
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials('Instagram','mburupavl','paulmburu08@gmail.com','think4562')
+        test_credentials.save_credentials()
+
+        self.new_credentials.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_find_by_name(self):
+        '''
+        test to check if we can find an account credentials by account_name and display information
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials('instagram','mburupavl','paulmburu08@gmail.com','think4562')
+        test_credentials.save_credentials()
+
+        found_credentials = Credentials.find_by_name('instagram')
+
+        self.assertEqual(found_credentials.username,test_credentials.username)
+
+    def test_display_credentials(self):
+        '''
+        method that returns a list of all account credentials saved
+        '''
+
+        self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
