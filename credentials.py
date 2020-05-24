eeb34e35d8961d121a00895b6108991941b1322d
paulmburu08 <paulmburu08@gmail.com>
2020-05-24 22:47:56 +0300

    add changes

--- credentials.py
+++ credentials.py
@@ -1,9 +1,11 @@
-class :
+from user import User
+
+class Credentials(User):
     '''
     class that generate new instances of credentials
     '''
 
-    credentials = []
+    credentials_list = []
 
     def __init__(self,first_name,last_name,email,password):
 
