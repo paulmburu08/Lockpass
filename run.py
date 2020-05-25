7bf62b93cb1f51450d6216d4ff6719542cfbd3e1
paulmburu08 <paulmburu08@gmail.com>
2020-05-25 09:56:23 +0300

    add changes

--- run.py
+++ run.py
@@ -2,6 +2,25 @@
 from user import User
 from credentials import Credentials
 
+def create_account(first_name,last_name,email,password):
+    '''
+    Function to create a new lockpass account
+    '''
+
+    new_user = User(first_name,last_name,email,password)
+    return new_user
+
+def save_account(account):
+    '''
+    Function to save user account
+    '''
+    account.save_account()
+
+def delete_account(account):
+    '''
+    Function to delete user account
+    '''
+    account.delete_account()
 
 
 
