ecbb2a480fe7b04e1ae04b8d9b1b48e9d71ae88b
paulmburu08 <paulmburu08@gmail.com>
2020-05-25 11:26:45 +0300

    add changes

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 from user import User
 from credentials import Credentials
 
+#User
+
 def create_account(first_name,last_name,email,password):
     '''
     Function to create a new lockpass account
@@ -10,18 +12,51 @@ def create_account(first_name,last_name,email,password):
     new_user = User(first_name,last_name,email,password)
     return new_user
 
-def save_account(account):
+def save_accounts(account):
     '''
     Function to save user account
     '''
     account.save_account()
 
-def delete_account(account):
+def del_account(account):
     '''
     Function to delete user account
     '''
     account.delete_account()
 
+#Credentials
+
+def create_credentials(account_name,username,email,password):
+    '''
+    Function to create new account credentials
+    '''
+
+    new_credentials = Credentials(account_name,username,email,password)
+    return new_credentials
+
+def save_credential(credentials):
+    '''
+    Function to save account credentials
+    '''
+    credentials.save_credentials()
+
+def del_credentials(credentials):
+    '''
+    Function to delete account credentials
+    '''
+    credentials.delete_credentials()
+
+def find_credentials(name):
+    '''
+    Function that finds credentials by account_name and returns credentials
+    '''
+    return Credentials.find_by_name(name)
+
+def display_credential()
+    '''
+    Function that returns all the saved account credentials
+    '''
+    Credentials.display_credentials()
 
 
 
